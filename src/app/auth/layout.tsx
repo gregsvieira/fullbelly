027117c9bda580110c756e3f32174c2529365b99
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-12-15 22:30:20 -0300

    update: layout pages
    change icon from utensils to donut
    removed text of footer
    removed text of auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,4 @@
-import { Utensils } from 'lucide-react';
+import { Donut } from 'lucide-react';
 
 export default function AuthLayout({
   children,
@@ -10,17 +10,16 @@ export default function AuthLayout({
 
         <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted pd-10 text-muted-foreground">
           <div className="flex items-center gap-3 text-lg font-medium text-foreground">
-            <Utensils className="h-5 w-5" />
-            <span>full belly</span>
+            <Donut className="h-5 w-5" />
+            <span>FullBelly</span>
           </div>
           <footer>
-            Painel do parceiro &copy; full belly - {new Date().getFullYear()}
+            &copy; FullBelly - {new Date().getFullYear()}
           </footer>
         </div>
 
         
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-2xl font-bold">Authentication</h1>
+      <div className="relative flex flex-col justify-center items-center">
         <div className="mt-4">{children}</div>
       </div>
 
