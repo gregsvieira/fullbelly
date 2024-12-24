1ddc47a3f704aad30a71b670094f6a4035303d9b
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2024-12-23 21:26:57 -0300

    refactor: change to OrderTableRow component

--- src/app/(logged-in)/orders/page.tsx
+++ src/app/(logged-in)/orders/page.tsx
@@ -1,20 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { ArrowRight, Search, X } from "lucide-react";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { OrderTableRow } from "./order-table-row";
+import { OrderTableFilters } from "./order-table-filters";
+import { Pagination } from "@/components/pagination";
 
 export default function Dashboard() {
   return (
     <>
         <div className="flex flex-col gap-4">
           <h1 className="text-3xl font-bold tracking-tight">Orders</h1>
-        </div>
+        
 
         <div className="space-y-2.5">
-          <form className="flex items-center gap-2">
-            <span className="text-sm font-semibold">Filters</span>
-            <Input placeholder="Client's Name" className="h-8 w-[320px]" />
-          </form>
+          <OrderTableFilters />
           
           <div className="border rounded-md">
             <Table>
@@ -33,50 +30,16 @@ export default function Dashboard() {
 
               <TableBody>
                 {Array.from({ length: 10 }).map((_, i) => {
-                  return (
-                    <TableRow key={i}>
-                      <TableCell>
-                        <Button variant="outline" size="xs">
-                          <Search className="h-3 w-3" />
-                          <span className="sr-only">Order Details</span>
-                        </Button>
-                      </TableCell>
-                      <TableCell className="font-mono text-xs font-medium">
-                        8e21sa612sdczkxla
-                      </TableCell>
-                      <TableCell className="text-muted-foreground">
-                        15 minutes ago
-                      </TableCell>
-                      <TableCell>
-                        <div className="flex items-center gap-2">
-                          <span className="h-2 w-2 rounded-full bg-slate-400" />
-                          <span className="font-medium text-muted-foreground">Pending</span>
-                        </div>
-                      </TableCell>
-                      <TableCell className="font-medium">Gregory Vieira</TableCell>
-                      <TableCell className="font-medium">
-                        R$ 149,90
-                      </TableCell>
-                      <TableCell>
-                        <Button variant="outline" size="xs">
-                          <ArrowRight className="mr-2 h-3 w-3" />
-                          Approve
-                        </Button>
-                      </TableCell>
-                      <TableCell>
-                        <Button variant="ghost" size="xs">
-                          <X className="mr-2 h-3 w-3" />
-                          Cancel
-                        </Button>
-                      </TableCell>
-                  </TableRow>
-                  )
+                  return <OrderTableRow key={i} />
                 })}
               </TableBody>
             </Table>
+
           </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
-        </>
+      </div>
+      </>
    
   );
 }
